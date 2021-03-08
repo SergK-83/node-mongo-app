772dd1e9452e5eb741551002adb0fdc8dc482533
Sergey Kryukov <krus-83@mail.ru>
2021-03-08 21:45:29 +0700

    git init

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+	defaultLayout: 'main',
+	extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(todoRoutes);
+
+async function start() {
+	try {
+		await mongoose.connect('mongodb+srv://serg:1q2w3e@cluster0.j0row.mongodb.net/todos', {
+			useNewUrlParser: true,
+			useFindAndModify: false
+		});
+		app.listen(PORT, () => {
+			console.log('Server has been started...');
+		});
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+start();
