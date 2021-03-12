375237dc1b81a6f3c92c0fa3e405dcf9f63c8b7e
Sergey Kryukov <krus-83@mail.ru>
2021-03-12 21:36:16 +0700

    fetch request updated

--- public/main.js
+++ public/main.js
@@ -3,22 +3,24 @@
 const todoRemoveBtnList = document.querySelectorAll('.todo__remove');
 
 todoRemoveBtnList.forEach((btn) => {
-	btn.addEventListener('click',   async function (e) {
+	btn.addEventListener('click', async function (e) {
 		e.preventDefault();
 
 		const todo = this.closest('.todo');
 		const todoId = todo.querySelector('input[name=id]').value;
 
-		 let promise = await fetch('/remove', {
+		let promise = await fetch('/remove', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({id: todoId})
-		}).then(response => {
-			if (response.redirected) {
-				document.location = response.url;
-			}
-		 });
+		})
+			.then(response => {
+				if (response.redirected) {
+					document.location = response.url;
+				}
+			})
+			.catch((err) => console.log(err));
 	});
 });
